a287e28abe62454c98e4670c6036921a50bf0c4c
Idian Capozzoli <idiancapozzoli@gmail.com>
2023-06-21 20:50:12 -0300

    Add tempo de vida da explosão

--- app/src/main/kotlin/galaxyraiders/core/game/Explosion.kt
+++ app/src/main/kotlin/galaxyraiders/core/game/Explosion.kt
@@ -2,18 +2,21 @@ package galaxyraiders.core.game
 
 import galaxyraiders.core.physics.Point2D
 import galaxyraiders.core.physics.Vector2D
+import java.util.Timer
 
 class Explosion(
   initialPosition: Point2D,
   //initialVelocity: Vector2D,
   radius: Double,
   mass: Double
-) : SpaceObject("Explosion", '@', initialPosition, Vector2D(dx = 1.0, dy = 1.0), radius, mass) {
-  var is_triggered: Boolean = false
+) : SpaceObject("Explosion", '@', initialPosition, Vector2D(1.0, 1.0), radius, mass) {
+  var is_triggered: Boolean = true
   
-  fun explode( ){
-    // acontece a explosao de alguma forma
-    is_triggered = true
+  var timer: Timer = Timer("schedule", true);
 
+  init {
+    timer.schedule(3000) {
+        is_triggered = false
+    }
   }
 }
